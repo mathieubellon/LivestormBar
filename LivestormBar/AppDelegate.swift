a3f3e4dd1773f8b4496e35c5f02f20a3d8e11588
Mathieu Bellon <1445897+mathieubellon@users.noreply.github.com>
2022-04-13 15:50:00 +0200

    Window management kind of working, imporving now

--- LivestormBar/AppDelegate.swift
+++ LivestormBar/AppDelegate.swift
@@ -13,10 +13,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     var statusItem: NSStatusItem?
     var popOver = NSPopover()
+    var statusBarItem: StatusBarItemController!
+    var preferencesWindow = NSWindowController()
+
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        createMenuBar()
+        statusBarItem = StatusBarItemController()
+        statusBarItem.setAppDelegate(appdelegate: self)
+        
+        let mainStoryboard = NSStoryboard.init(name: NSStoryboard.Name("Main"), bundle: nil)
+        preferencesWindow = mainStoryboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier("mainWindowController")) as! NSWindowController
+        preferencesWindow.close()
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -31,26 +40,92 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     
-    func createMenuBar() -> Void {
-        let menuView = MenuBarView()
-        popOver.behavior = .transient
-        popOver.animates = true
-        popOver.contentViewController = NSViewController()
-        popOver.contentViewController?.view = NSHostingView(rootView: menuView)
-        
-        // Creating status bar button
-        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        
-        // Safe check if statusbar is created
-        guard let logo = NSImage(named: NSImage.Name("logo-primary-svg")) else { return }
+    class StatusBarItemController: NSObject, NSMenuDelegate {
+        var statusItem: NSStatusItem!
+        var statusItemMenu: NSMenu!
+        var menuIsOpen = false
+        weak var appdelegate: AppDelegate!
 
-        let resizedLogo = NSImage(size: NSSize(width: 18, height: 18), flipped: false) { (dstRect) -> Bool in
-            logo.draw(in: dstRect)
-            return true
+        
+        func enableButtonAction() {
+            let button: NSStatusBarButton = statusItem.button!
+            // Safe check if statusbar is created
+            guard let logo = NSImage(named: NSImage.Name("logo-primary-svg")) else { return }
+
+            let resizedLogo = NSImage(size: NSSize(width: 18, height: 18), flipped: false) { (dstRect) -> Bool in
+                logo.draw(in: dstRect)
+                return true
+            }
+            button.image = resizedLogo
+            button.target = self
+            button.action = #selector(statusMenuBarAction)
+            button.sendAction(on: [NSEvent.EventTypeMask.rightMouseDown, NSEvent.EventTypeMask.leftMouseUp, NSEvent.EventTypeMask.leftMouseDown])
+            menuIsOpen = false
         }
-        if let MenuButton = statusItem?.button{
-            MenuButton.image = resizedLogo
-            MenuButton.action = #selector(MenuButtonToggle)
+        
+        override
+        init() {
+            super.init()
+
+            statusItem = NSStatusBar.system.statusItem(
+                withLength: NSStatusItem.variableLength
+            )
+
+            statusItemMenu = NSMenu(title: "LivestormBar in Status Bar Menu")
+            statusItemMenu.delegate = self
+            statusItemMenu.addItem(NSMenuItem.separator())
+            statusItemMenu.addItem(withTitle: "Mes prochaine réunions",
+                         action: #selector(NSText.selectAll(_:)), keyEquivalent: "")
+            statusItemMenu.addItem(NSMenuItem.separator())
+            statusItemMenu.addItem(withTitle: "Créer une réunion",
+                         action: #selector(NSText.selectAll(_:)), keyEquivalent: "")
+            statusItemMenu.addItem(withTitle: "Plus d'actions",
+                         action: #selector(NSText.copy(_:)), keyEquivalent: "c")
+            statusItemMenu.addItem(NSMenuItem.separator())
+            statusItemMenu.addItem(
+                withTitle: "Préférence",
+                action: #selector(AppDelegate.openPreferencesWindow),
+                keyEquivalent: ","
+            )
+
+            statusItemMenu.addItem(
+                withTitle: "Quitter LivestormBar",
+                action: #selector(AppDelegate.quit),
+                keyEquivalent: "q"
+            )
+
+            enableButtonAction()
+        }
+
+        @objc
+        func menuWillOpen(_: NSMenu) {
+            menuIsOpen = true
+        }
+
+        @objc
+        func menuDidClose(_: NSMenu) {
+            // remove menu when closed so we can override left click behavior
+            statusItem.menu = nil
+            menuIsOpen = false
+        }
+        func setAppDelegate(appdelegate: AppDelegate) {
+            self.appdelegate = appdelegate
+        }
+        @objc
+        func statusMenuBarAction(sender _: NSStatusItem) {
+            NSLog("User clicked menuBar to open")
+            if !menuIsOpen, statusItem.menu == nil {
+                let event = NSApp.currentEvent
+
+                // Right button click
+                if event?.type == NSEvent.EventType.rightMouseUp {
+                    print("rightclikc")
+                } else if event == nil || event?.type == NSEvent.EventType.leftMouseDown || event?.type == NSEvent.EventType.leftMouseUp {
+                    // show the menu as normal
+                    statusItem.menu = statusItemMenu
+                    statusItem.button?.performClick(nil) // ...and click
+                }
+            }
         }
     }
     
@@ -157,6 +232,49 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
         // If we got here, it is time to quit.
         return .terminateNow
+    }
+    
+    @objc
+    func openPreferencesWindow(_: NSStatusBarButton?) {
+        NSLog("Open preferences window")
+        preferencesWindow.showWindow(self)
+        //
+        
+        //        if preferencesWindow != nil {
+//            preferencesWindow.close()
+//        }
+//        preferencesWindow = NSWindow(
+//            contentRect: NSRect(x: 0, y: 0, width: 700, height: 610),
+//            styleMask: [.closable, .titled, .resizable],
+//            backing: .buffered,
+//            defer: false
+//        )
+//
+//        preferencesWindow.title = "Bar Settings"
+//
+//
+//        let storyboard: NSStoryboard = NSStoryboard(name: "Main", bundle: nil)
+//
+//
+//
+//
+//
+//        preferencesWindow.makeKeyAndOrderFront(nil)
+//        // allow the preference window can be focused automatically when opened
+//        NSApplication.shared.activate(ignoringOtherApps: true)
+//
+//        let controller = NSWindowController(window: preferencesWindow)
+//        controller.showWindow(self)
+//
+//        preferencesWindow.center()
+//        preferencesWindow.orderFrontRegardless()
+    }
+
+    
+    @objc
+    func quit(_: NSStatusBarButton) {
+        NSLog("User click Quit")
+        NSApplication.shared.terminate(self)
     }
 
 }
