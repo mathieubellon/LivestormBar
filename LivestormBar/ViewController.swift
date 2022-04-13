3c82b62e0dfc9e11f524ace53a2a5115a7b42f82
Mathieu Bellon <1445897+mathieubellon@users.noreply.github.com>
2022-04-13 10:11:08 +0200

    Initial Commit

---
+++ LivestormBar/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LivestormBar
+//
+//  Created by Mathieu Bellon on 13/04/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
