3fc240d0390b96dc2c2d94852088581c87a9ef04
Mathieu Bellon <1445897+mathieubellon@users.noreply.github.com>
2022-05-05 07:49:50 +0200

    Prepare Sparkle 3

--- LivestormBar/AppDelegate.swift
+++ LivestormBar/AppDelegate.swift
@@ -49,7 +49,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCenterDele
             openNextEvent()
         }
         
-        openOnboardingWindow()
+        //openOnboardingWindow()
     }
     
     
